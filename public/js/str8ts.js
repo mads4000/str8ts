7c52331a659917bfe518aa5f0e9477d19b970a42
Markus Walbrun <markus.walbrun@sueddeutsche.de>
2020-09-29 12:47:14 +0200

    Draw the grid

--- public/js/str8ts.js
+++ public/js/str8ts.js
@@ -1,5 +1,31 @@
 import { qs, qsa, domReady } from './utils';
+import data from '../data/riddle.json';
+
+const createGrid = () => {
+  console.log('Hello str8ts', data)
+
+  const playground = qs('.playground');
+
+
+  const grid = data.reduce((rows, row) => {
+    const rowHtml = row.reduce((columns, column) => {
+      const input = column.value ?
+        `<input type="text" autocomplete="off" autocorrect="off" inputmode="verbatim" spellcheck="false" disabled="true" value="${column.value}"/>` :
+        `<input type="text" autocomplete="off" autocorrect="off" inputmode="verbatim" spellcheck="false" />`
+
+      return `${columns}
+        <li class="cell ${column.color}">
+          ${input}
+        </li>`
+    }, '');
+    return `${rows}<ul class="row">${rowHtml}</ul>`;
+  }, '');
+
+
+  playground.innerHTML = grid;
+
+};
 
 domReady(() => {
-  console.log('Hello str8ts')
+  createGrid();
 });
