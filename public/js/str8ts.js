213e3d39feed6244e0b62d6b01cd57160eb8b228
Markus Walbrun <markus.walbrun@sueddeutsche.de>
2020-09-29 15:50:01 +0200

    Added some logic

--- public/js/str8ts.js
+++ public/js/str8ts.js
@@ -1,31 +1,56 @@
 import { qs, qsa, domReady } from './utils';
 import data from '../data/riddle.json';
 
-// const validateRow = () => {
+const hasDuplicates = array => new Set(array).size !== array.length;
+const validValue = value => Number.isInteger(value) && value > 0 && value <= 9;
 
-// };
+const validateRow = () => {
+  const columnValues = [];
+  qsa(`[data-row-idx="${index}"] > [data-validate="true"]`).forEach(elem => columnValues.push(Number.parseInt(elem.value)));
 
-// const validateColumn = () => {
+  let validValues = true;
+  columnValues.forEach(elem => validValues = validValues && validValue(elem));
+  
+  return validValues && !hasDuplicates(columnValues);
+};
 
-// };
+const validateColumn = (index) => {
+  const columnValues = [];
+  qsa(`[data-column-idx="${index}"] > [data-validate="true"]`).forEach(elem => columnValues.push(Number.parseInt(elem.value)));
+
+  let validValues = true;
+  columnValues.forEach(elem => validValues = validValues && validValue(elem));
+
+  return validValues && !hasDuplicates(columnValues);
+};
 
 
-// const validate = () => {
-//   const gridValues
-// };
+const validate = () => {
+  for (let i = 0; i < 8; i++) {
+    const result = validateColumn(i) && validateRow(i);
+
+    if (!result) {
+      qs('.error').classList.remove('hidden');
+    }
+  }
+};
 
 
 const createGrid = () => {
-  console.log('Hello str8ts', data)
-
   const playground = qs('.playground');
 
 
   const grid = data.reduce((rows, row, rowIndex) => {
     const rowHtml = row.reduce((columns, column, columnIndex) => {
-      const input = column.value ?
-        `<input class="cell-input" type="number" inputmode="verbatim" min="0" max="9" disabled="true" value="${column.value}"/>` :
-        `<input class="cell-input" type="number" inputmode="verbatim" min="0" max="9" />`
+      let input = '';
+
+      if (column.value) {
+        input = `<input class="cell-input" type="number" inputmode="verbatim" min="0" max="9" disabled="true" data-validate="true" value="${column.value}"/>`
+      } else if (column.color === 'black') {
+        input = `<input class="cell-input" type="number" inputmode="verbatim" min="0" max="9" disabled="true"/>`;
+      } else {
+        input = `<input class="cell-input" type="number" inputmode="verbatim" min="0" max="9" data-validate="true" />`
+      }
 
       return `${columns}
         <li class="cell ${column.color}" data-row-idx="${rowIndex}" data-column-idx="${columnIndex}">
@@ -41,4 +66,26 @@ const createGrid = () => {
 
 domReady(() => {
   createGrid();
+
+  qs(`.validate-button`).addEventListener('click', () => {
+    validate();
+  });
+
+  qsa('.cell-input').forEach(input => {
+    input.addEventListener('keydown', () => {
+      qs('.error').classList.add('hidden'); 
+    })
+  });
+
+  qsa('.cell-input').forEach(input => {
+    input.addEventListener('keyup', event => {
+      const inputElement = event.currentTarget;
+
+      if (inputElement && inputElement.value.length > 1) {
+        inputElement.classList.add('unsure');
+      } else {
+        inputElement.classList.remove('unsure');
+      }
+    })
+  });
 });
